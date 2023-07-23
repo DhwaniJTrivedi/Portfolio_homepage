8d380cf8bc6ef7589bcce10c0b8408f25a1eef37
DhwaniJTrivedi <127465429+DhwaniJTrivedi@users.noreply.github.com>
2023-07-23 18:16:47 -0400

    added flex and trying media query

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrollToContent(tabNumber) {
+    const content = document.getElementById(`content${tabNumber}`);
+    const contentOffset = content.getBoundingClientRect().top;
+    const navbarHeight = 100; // Adjust this to your navbar height if applicable
+    const scrollOptions = {
+      behavior: "smooth",
+      block: "start"
+    };
+    window.scrollBy({
+      top: contentOffset - navbarHeight,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
